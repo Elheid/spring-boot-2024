e1cefed85a738d08d463dc16fa8a8ad87ddf8054
Elheid <Elfieleintsvard@mail.ru>
2024-03-16 21:32:26 +0500

    refactoring3

--- src/main/java/com/students/demo/MyService.java
+++ src/main/java/com/students/demo/MyService.java
@@ -17,29 +17,20 @@ public class MyService {
 
     //DI через поле
    @Autowired
-   @NonFinal MyBeanFirst myBean1;
+   @NonFinal MyInterface myBeanFirst;
 
     // DI через setter
-    @NonFinal MyInterface myBean2;
+    @NonFinal MyInterface myBeanSecond;
     @Autowired
-    private void setMyService2(MyBeanSecond myBean2) {
-        this.myBean2 = myBean2;
+    public void setMyService2(MyInterface myBeanSecond) {
+        this.myBeanSecond = myBeanSecond;
         log.info("Произошла инициализация бина через setter");
-    }
-
-    // DI через конструктор
-
-    private final MyInterface myBean3;
-    private MyService(MyBeanThird myBean3) {
-        this.myBean3 = myBean3;
-        log.info("Произошла инициализация бина по конструктору");
     }
 
 
     public void doSomething() {
         log.info("Service ding smth");
-        myBean1.doSomething();
-        myBean2.doSomething();
-        myBean3.doSomething();
+        myBeanFirst.doSomething();
+        myBeanSecond.doSomething();
     }
 }
