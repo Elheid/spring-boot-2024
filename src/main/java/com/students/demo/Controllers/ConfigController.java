fe720f582d3d4a3fc138443eb996cdfcc8cf2914
Elheid <Elfieleintsvard@mail.ru>
2024-04-03 18:53:16 +0500

    firstVersion

---
+++ src/main/java/com/students/demo/Controllers/ConfigController.java
@@ -0,0 +1,36 @@
+package com.students.demo.Controllers;
+
+import com.students.demo.MyServiceSecond;
+import com.students.demo.config.MyProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ConfigController {
+
+    private final MyProperties myProperties;
+    private MyServiceSecond service;
+
+    @Autowired
+    public ConfigController(MyProperties myProperties, MyServiceSecond service) {
+        this.myProperties = myProperties;
+        this.service = service;
+    }
+
+    @GetMapping("/config")
+    public String getConfig() {
+       // return service.CreateConfigResponse(myProperties);
+        return  CreateConfigResponse(myProperties);
+    }
+
+    public String CreateConfigResponse(MyProperties myProperties){
+        String response = "";
+        response+="List values: "+(myProperties.getListValues());
+        response+="\nEnvironment variable: "+myProperties.getEnvironmentVariable();
+        return response;
+    }
+
+}
